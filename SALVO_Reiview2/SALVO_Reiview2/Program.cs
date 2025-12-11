79d6554120833e354a5e033e4869fe40b345ca21
ACT-STUDENT <ACT-STUDENT@LAB2-PC46>
2025-12-11 16:30:29 +0800

    asddsfdfsds

---
+++ SALVO_Reiview2/SALVO_Reiview2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SALVO_Reiview2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("\tAREA COMPUTATION PROGRAM\n");
+
+            Console.WriteLine("Choose a shape:\n");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("1 - Circle");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("2 - Rectangle\n");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("Enter your choice: ");
+
+
+            int choice = int.Parse(Console.ReadLine());
+
+            Shape shape = null;
+
+            if (choice == 1)
+            {
+                Circle c = new Circle();
+                Console.Write("Enter color: ");
+                c.Color = Console.ReadLine();
+
+                Console.Write("Enter radius: ");
+                c.Radius = double.Parse(Console.ReadLine());
+
+                shape = c;
+            }
+            else if (choice == 2)
+            {
+                Rectangle r = new Rectangle();
+                Console.Write("Enter color: ");
+                r.Color = Console.ReadLine();
+
+                Console.Write("Enter width: ");
+                r.Width = double.Parse(Console.ReadLine());
+
+                Console.Write("Enter height: ");
+                r.Height = double.Parse(Console.ReadLine());
+
+                shape = r;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid choice!\n");
+                return;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\n--- RESULT ---\n");
+            Console.WriteLine("Color: " + shape.Color);
+            Console.WriteLine("Area: " + shape.GetArea());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SALVO_Reiview2/SALVO_Reiview2/Shape.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SALVO_Reiview2
+{
+   public class Shape
+    {
+        public string Color { get; set; }
+
+        public virtual double GetArea()
+        {
+            return 0;
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+    }
+}
