dd46ac6d06b6fcfb71e648c848f1f10f7ef16703
riri <ryancarlsalvo05@gmail.com>
2026-01-15 16:11:02 +0800

    Add files via upload

---
+++ SALVO_Act1/SALVO_Act1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SALVO_Act1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare Array 
+            string[] Countries = new string[5];
+
+            // User Input
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("  Please enter your 5 favorite countries: \n");
+
+            for (int idx = 0; idx < Countries.Length; idx++)
+            {
+                Console.Write($"Countrie #{idx + 1}: ");
+                Countries[idx] = Console.ReadLine();
+            }
+
+            // sort and Display on user choice
+            Array.Sort(Countries);
+
+            // Display user favorite countries in ascending order
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("\n -----Here is the list of your favorite Countries in Ascending order-----\n");
+
+            foreach (string s in Countries)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
