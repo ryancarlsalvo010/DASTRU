02085db706f6d9428743974ece9957875089a01a
riri <ryancarlsalvo05@gmail.com>
2026-01-15 16:18:56 +0800

    Add files via upload

---
+++ Salvo_Act4/Salvo_Act4/Countries.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salvo_Act4
+{
+     class Countries
+    {
+        public string Name { get; set; }
+        public string Population { get; set; }
+        public string TouristAttraction { get; set; }
+
+        // Parametarized Constructor
+        public Countries(string name, string population, string tourist)
+        {
+            Name = name;
+            Population = population;
+            TouristAttraction = tourist;
+        }
+
+        public virtual void Display() //Virtual World
+        {
+            Console.WriteLine($"Countrie Name: {Name}");
+            Console.WriteLine($"How many population: {Population}");
+            Console.WriteLine($"Top 1 Famous Tourist Attraction: {TouristAttraction}");
+
+        }
+    }
+    class Continent : Countries // Derived Class
+    {
+        public string Area { get; set; } // Addtional Property 
+
+        // Parametarized Constructor
+        public Continent(string name, string population, string tourist, string area) : base(name, population, tourist)
+        {
+            Area = area;
+        }
+        public override void Display()
+        {
+            Console.WriteLine("\n----- Continet Details-----");
+            Console.WriteLine($"Area: {Area}");
+            base.Display();
+        }
+    }
+
+    class Government : Countries
+    {
+        public string President { get; set; }
+
+        // Parametarized Constructor
+        public Government(string name, string population, string tourist, string president) : base(name, population, tourist)
+        {
+            President = president;
+        }
+        public override void Display()
+        {
+            Console.WriteLine("\n----- Government Details-----");
+            Console.WriteLine($"President: {President}");
+            base.Display();
+        }
+    }
+}
---
+++ Salvo_Act4/Salvo_Act4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salvo_Act4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            
+            {
+                // Create an array of Countries objects
+                Countries[,] countries = new Countries[2, 2];
+
+                // Assign values for each object
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                //ROW 0:
+                countries[0, 0] = new Continent("Canada", "41.5 to 41.7 million people", "Niagara Falls", "9.985 million km²");
+                countries[0, 1] = new Continent("Italy", "59 million people", "the Colosseum in Rome", "301,340 square kilometers");
+
+                //ROW 1:
+                countries[1, 0] = new Government("Brazil", "213 million people", " Christ the Redeemer statue in Rio de Janeiro", "Luiz Inácio Lula da Silva");
+                countries[1, 1] = new Government("England", "57.1 million people", "the Tower of London", "King Charles III (Head of State)");
+
+                // Display Information for each Countries
+                for (int i = 0; i < countries.GetLength(0); i++)
+                {
+                    for (int j = 0; j < countries.GetLength(1); j++)
+                    {
+                        countries[i, j].Display();
+                    }
+                }
+                Console.ReadKey();
+            }
+    }
+}
